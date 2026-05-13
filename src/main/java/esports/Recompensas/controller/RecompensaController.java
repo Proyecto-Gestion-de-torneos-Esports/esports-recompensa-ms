861c8d4bbb6703f90872745ee348310358df3b36
Amaro <godoyamaro59@gmail.com>
2026-05-12 22:11:13 -0400

    avance en el controller

---
+++ src/main/java/esports/Recompensas/controller/RecompensaController.java
@@ -0,0 +1,34 @@
+package esports.Recompensas.controller;
+
+import esports.Recompensas.dto.RecompensaResponseDTO;
+import esports.Recompensas.service.RecompensaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+
+public class RecompensaController {
+
+    private final RecompensaService recompensaService;
+
+@GetMapping
+public ResponseEntity<List<RecompensaResponseDTO>> obtenerTodos() {
+    return ResponseEntity.ok(recompensaService.obtenerTodos());
+  }
+  @GetMapping("/{id}")
+    public ResponseEntity<RecompensaResponseDTO> buscarPorId(@PathVariable Long id){
+    return ResponseEntity.ok(recompensaService.buscarPorid(id));
+  }
+
+  @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarRecompensa(@PathVariable Long id){
+    recompensaService.eliminarRecompensa(id);
+    return ResponseEntity.noContent().build();
+  }
+
+}
