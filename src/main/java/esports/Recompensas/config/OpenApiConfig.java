316b47aa2ec53340e01a9175ff47d76d0bef2c2f
Leonel Barrientos <l.a.barrientos.riquelme@gmail.com>
2026-06-19 01:16:50 -0400

    fix: se arreglo el Swagger para que pueda recibir peticiones

--- src/main/java/esports/Recompensas/config/OpenApiConfig.java
+++ src/main/java/esports/Recompensas/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package esports.Recompensas.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +12,23 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("API de Recompensas - Plataforma Esports")
                         .version("1.0.0")
-                        .description("Microservicio encargado de gestionar el cálculo, división y asignación de premios a los equipos participantes de los torneos."));
+                        .description("Microservicio encargado de gestionar el cálculo, división y asignación de premios a los equipos participantes de los torneos."))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .description("TOKEN JWT")
+                        )
+                );
     }
 
 }
